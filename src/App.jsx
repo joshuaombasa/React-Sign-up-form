48b9ed2c131d7f4e1e3c694fe3c936af797c7c5e
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-04-19 17:32:55 +0300

    added jsx

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+     <form className="signup-form">
+        <input 
+        type="email" 
+        className="email" 
+        />
+        <input 
+        type="password" 
+        className="password" 
+        />
+        <input 
+        type="password" 
+        className="confpassword" 
+        />
+        <div className="checkbox-container">
+              <input 
+              type="checkbox" 
+              name="" 
+        
+              id="isJoinNewsLetter" 
+              />
+              <label htmlFor="isJoinNewsLetter"></label>
+        </div>
+        <button className='sign-up-btn'>Sign Up</button>
+     </form>
+    </div>
+  )
+}
+
+export default App
