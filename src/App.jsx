9cd8b6f4c400036741898a0655d85236da4dbed4
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-04-19 18:16:29 +0300

    updated jsx

--- src/App.jsx
+++ src/App.jsx
@@ -25,9 +25,17 @@ function App() {
     })
   }
 
+  function handleSubmit(event) {
+    event.preventDefault()
+    const successMsg = "Successfully signed up"
+    const failMsg = "passwords to not match"
+    formData.password === formData.confpassword ? console.log(successMsg) : console.log(failMsg)
+    formData.isJoinNewsLetter? console.log("Thanks for signing up for our newsletter!") : ''
+  }
+
   return (
     <div className="App">
-     <form className="signup-form">
+     <form className="signup-form" onSubmit={handleSubmit}>
         <input 
         type="email" 
         className="email" 
